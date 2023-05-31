2537485e3845b1909d630e49ebd3f22a977e2eef
rishikesh devare <rishidevare051@gmail.com>
2023-05-31 22:15:27 +0530

    done

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -4,60 +4,83 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+
 
   @override
-  
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  Completer<GoogleMapController> _controller = Completer();
-  final CameraPosition _Googleposition = const CameraPosition(
-      target: LatLng(19.11926985062429, 73.00412107950969),
-      zoom: 17
+  final Completer<GoogleMapController> _controller = Completer();
+
+  List<Marker> _markers = <Marker>[];
+
+  static const CameraPosition _kGooglePlex =  CameraPosition(
+    target: LatLng(33.6844, 73.0479),
+    zoom: 14,
   );
-    final List<Marker> _marker = [];
-    final List<Marker> _list = const[
-      Marker(markerId: MarkerId('1'),
-        position: LatLng(19.11926985062429, 73.00412107950969),
-        infoWindow: InfoWindow(
-          title: 'Myposition'
-        )
-      ),
-      Marker(markerId: MarkerId('1'),
-          position: LatLng(21.11926985062429, 73.00412107950969),
-          infoWindow: InfoWindow(
-              title: 'Myposition'
-          )
-      )
-    ];
-
 
 
   @override
-  void initState(){
+  void initState() {
+    // TODO: implement initState
     super.initState();
-    _marker.addAll(_list);
-  }
-
-  @override
-
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      body: GoogleMap(
-        onMapCreated: (GoogleMapController controller){
-          _controller.complete(controller);
-        },
-        mapType: MapType.hybrid,
-        myLocationEnabled: true,
-        initialCameraPosition: _Googleposition,
-        markers: Set.of(_marker),
-
-
-      ),
+    _markers.add(
+        Marker(
+            markerId: MarkerId('SomeId'),
+            position: LatLng(37.42796133580664, -122.085749655962),
+            infoWindow: InfoWindow(
+                title: 'The title of the marker'
+            )
+        )
     );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+          initialCameraPosition: _kGooglePlex,
+          mapType: MapType.normal,
+          zoomControlsEnabled: true,
+          zoomGesturesEnabled: true,
+          myLocationButtonEnabled: true,
+          myLocationEnabled: true,
+          trafficEnabled: false,
+          rotateGesturesEnabled: true,
+          buildingsEnabled: true,
+          markers: Set<Marker>.of(_markers),
+          onMapCreated: (GoogleMapController controller){
+            _controller.complete(controller);
+          },
+        ),
+      );
+
+  }
 }
+
+// List<Marker> list = const [
+//   Marker(
+//       markerId: MarkerId('SomeId'),
+//       position: LatLng(33.6844, 73.0479),
+//       infoWindow: InfoWindow(
+//           title: 'The title of the marker'
+//       )
+//   ),
+//   Marker(
+//       markerId: MarkerId('SomeId'),
+//       position: LatLng( 33.738045,73.084488),
+//       infoWindow: InfoWindow(
+//           title: 'e-11 islamabd'
+//       )
+//   ),
+// ];
+//
+// @override
+// void initState() {
+//   // TODO: implement initState
+//   super.initState();
+//   _markers.addAll(
+//       list);
+// }
